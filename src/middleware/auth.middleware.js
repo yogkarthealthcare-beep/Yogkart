96f48b05efa32d6cafd5e9b402a12df6f54efd87
LAPTOP-C4HDSJL1 <vikasdotme@gmail.com>
2026-05-20 20:18:36 +0530

    LinkedIn Commit

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -6,10 +6,7 @@ const { unauthorized, forbidden } = require('../utils/response');
 const protect = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
-    if (!authHeader || !authHeader.startsWith('Bearer ')) {
-//      return unauthorized(res, 'No token provided');
-    }
-
+ 
     const token = authHeader.split(' ')[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
